9f0eff142e0f4fafa54fd66576aeda874a6bb25c
Tristen Nollman <tristen.nollman@gmail.com>
2021-12-22 00:32:16 -0500

    add testing framework and ability to exit qemu automatically after tests complete. Currently does not print results to host console.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![no_std] //prevents link to Rust std lib
 #![no_main] //disables default Rust entrypoints
+#![feature(custom_test_frameworks)] //allow custom test framework since Rust default needs stdlib
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"] //rename the exported main  from our test framework
+
 
 use core::panic::PanicInfo;
 mod vga_buffer;
@@ -17,6 +21,53 @@ static HELLO: &[u8] = b"Hello World!";
 pub extern "C" fn _start() -> ! {
     //Entrypoint since the Linker looks for a function
     // named _start (LLVM Linker) by default
-    panic!("This is a panic!");
+    println!("Hello world!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failure = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
+
+/*
+ Test Framework functions
+
+ */
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    if tests.len() == 0 {
+        return
+    }
+    println!("Running {} tests.", tests.len());
+    for test in tests {
+        test();
+    }
+
+    //exit after tests
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[test_case]
+fn trivial_assert() {
+    print!("Trivial Assertion... ");
+    assert_eq!(1, 1);
+    println!("[passed]");
 }
