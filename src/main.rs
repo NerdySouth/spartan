b4bf567341a84d2ea098d3bb968e6ed8524437eb
Tristen Nollman <tristen.nollman@gmail.com>
2021-12-22 02:16:32 -0500

    add integration tests framework. Next I will add support for should_panic tests

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,13 @@
 #![no_std] //prevents link to Rust std lib
 #![no_main] //disables default Rust entrypoints
 #![feature(custom_test_frameworks)] //allow custom test framework since Rust default needs stdlib
-#![test_runner(crate::test_runner)]
+#![test_runner(spartan::test_runner)]
 #![reexport_test_harness_main = "test_main"] //rename the exported main  from our test framework
 
 
 use core::panic::PanicInfo;
-mod vga_buffer;
-mod serial;
+use spartan::println;
 
-//called on panic
-#[cfg(not(test))]  //use this panic handler when not running tests
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    println!("{}", _info);
-    loop {}
-}
-
-#[cfg(test)] // use this panic handler when running tests
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    exit_qemu(QemuExitCode::Failure);
-    loop {}
-}
-
-static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle] // -> do not mangle name of this function
 pub extern "C" fn _start() -> ! {
@@ -40,60 +21,16 @@ pub extern "C" fn _start() -> ! {
     loop {}
 }
 
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failure = 0x11,
+//called on panic
+#[cfg(not(test))]  //use this panic handler when not running tests
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", _info);
+    loop {}
 }
 
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-
-/*
- Test Framework functions
-
- */
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        //type_name of Fn() is the function's name
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self(); //works since we require that self implements the Fn() trait
-        serial_println!("[passed]");
-    }
-}
-
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]) {
-    if tests.len() == 0 {
-        return
-    }
-    serial_println!("Running {} tests.", tests.len());
-    for test in tests {
-        test.run();
-    }
-
-    //exit after tests
-    exit_qemu(QemuExitCode::Success);
-}
-
-#[test_case]
-fn trivial_assert() {
-    assert_eq!(1, 1);
+#[cfg(test)] // use this panic handler when running tests
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    spartan::test_panic_handler(_info);
 }
---
+++ tests/basic_boot.rs
@@ -0,0 +1,33 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(spartan::test_runner)]
+#![reexport_test_harness_main= "test_main"]
+
+use core::panic::PanicInfo;
+use spartan::println;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    loop {}
+}
+
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> !{
+    loop{}
+}
+
+///test cases
+#[test_case]
+fn test_println() {
+    spartan::println!("test_println output");
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
